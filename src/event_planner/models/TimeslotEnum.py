af3914699454f72aa0ea7531f35a390740cc4a4e
Zane Cersovsky <zjcersovsky@gmail.com>
2017-09-12 23:26:10 -0500

    Added a type for timeslots

---
+++ src/event_planner/models/TimeslotEnum.py
@@ -0,0 +1,56 @@
+import enum
+class TimeslotEnum(enum.Enum):
+    timeslot_0 = 0
+    timeslot_1 = 1
+    timeslot_2 = 2
+    timeslot_3 = 3
+    timeslot_4 = 4
+    timeslot_5 = 5
+    timeslot_6 = 6
+    timeslot_7 = 7
+    timeslot_8 = 8
+    timeslot_9 = 9
+    timeslot_10 = 10
+    timeslot_11 = 11
+    timeslot_12 = 12
+    timeslot_13 = 13
+    timeslot_14 = 14
+    timeslot_15 = 15
+    timeslot_16 = 16
+    timeslot_17 = 17
+    timeslot_18 = 18
+    timeslot_19 = 19
+    timeslot_20 = 20
+    timeslot_21 = 21
+    timeslot_22 = 22
+    timeslot_23 = 23
+    timeslot_24 = 24
+    timeslot_25 = 25
+    timeslot_26 = 26
+    timeslot_27 = 27
+    timeslot_28 = 28
+    timeslot_29 = 29
+    timeslot_30 = 30
+    timeslot_31 = 31
+    timeslot_32 = 32
+    timeslot_33 = 33
+    timeslot_34 = 34
+    timeslot_35 = 35
+    timeslot_36 = 36
+    timeslot_37 = 37
+    timeslot_38 = 38
+    timeslot_39 = 39
+    timeslot_40 = 40
+    timeslot_41 = 41
+    timeslot_42 = 42
+    timeslot_43 = 43
+    timeslot_44 = 44
+    timeslot_45 = 45
+    timeslot_46 = 46
+    timeslot_47 = 47
+    @classmethod
+    def civil_time():
+        return "foo"
+    @classmethod
+    def military_time():
+        return "bar"

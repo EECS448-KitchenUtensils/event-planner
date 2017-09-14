b4e74744a3786130c72e170dd2b947e2192221cb
Armaan Amirani <armaanamirani@gmail.com>
2017-09-13 23:41:07 -0500

    Wording, removed unneccessary test comments

--- src/event_planner/views/routes.py
+++ src/event_planner/views/routes.py
@@ -65,16 +65,9 @@ def new_post():
 @app.route("/event/<event_id>", methods=['GET'])
 def show_event_get(event_id):
     """ GET - user view """
-    
+
+    #Get event by ID from DB and send to event view
     event = models.Event.query.filter(models.Event.id == event_id).first()
-
-
-    # events.where(event.id == event_id)
-    #event = {}
-    #event['name'] = 'Foobar rally'
-    #event['desc'] = 'fight for your right to bar with your foo'
-    #event['time'] = '6/9/6969'
-    #event['participants'] = [('Jenny Swepack', '1-4'), ('Gerald Moneybaby', '6-5')]
 
     #Create the dates to show in the template for iterator.
     daterange = ['12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM','3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM']

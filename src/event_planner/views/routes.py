6bb171a9ba0fe1ed36c55ee8bda0e74727054307
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-14 19:01:50 -0500

    Added a function to get a specific event in the route

--- src/event_planner/views/routes.py
+++ src/event_planner/views/routes.py
@@ -67,7 +67,7 @@ def show_event_get(event_id):
     """ GET - user view """
 
     #Get event by ID from DB and send to event view
-    event = models.Event.query.filter(models.Event.id == event_id).first()
+    event = get_event(event_id)
 
     #Create the dates to show in the template for iterator.
     daterange = ['12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM','3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM']
@@ -90,12 +90,8 @@ def show_event_post(event_id=None):
     #db.session.add(event)
     #db.session.commit()
 
-    # events.where(event.id == event_id)
-    event = {}
-    event['name'] = 'Foobar rally'
-    event['desc'] = 'fight for your right to bar with your foo'
-    event['time'] = '6/9/6969'
-    event['participants'] = [('Jenny Swepack', '1-4'), ('Gerald Moneybaby', '6-5')]
+    #Get event by ID from DB and send to event view
+    event = get_event(event_id)
 
     #Create the dates to show in the template for iterator.
     daterange = ['12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM','3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM']
@@ -107,12 +103,8 @@ def show_event_post(event_id=None):
 @app.route("/event/<event_id>/<event_auth_token>", methods=['GET'])
 def show_event_get_admin(event_id=None, event_auth_token=None):
     """ GET - admin view """
-    # events.where(event.id == event_id)
-    event = {}
-    event['name'] = 'Foobar rally'
-    event['desc'] = 'fight for your right to bar with your foo'
-    event['time'] = '6/9/6969'
-    event['participants'] = [('Jenny Swepack', '1-4'), ('Gerald Moneybaby', '6-5')]
+    #Get event by ID from DB and send to event view
+    event = get_event(event_id)
 
     #Create the dates to show in the template for iterator.
     daterange = ['12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM','3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM']
@@ -126,12 +118,8 @@ def show_event_post_admin(event_id=None, event_auth_token=None):
     """ POST - admin changes """
     # TODO pull in all data about the event and put in the database
 
-    # events.where(event.id == event_id)
-    event = {}
-    event['name'] = 'Foobar rally'
-    event['desc'] = 'fight for your right to bar with your foo'
-    event['time'] = '6/9/6969'
-    event['participants'] = [('Jenny Swepack', '1-4'), ('Gerald Moneybaby', '6-5')]
+    #Get event by ID from DB and send to event view
+    event = get_event(event_id)
 
     #Create the dates to show in the template for iterator.
     daterange = ['12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM','3:00 AM', '3:30 AM', '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM']
@@ -139,3 +127,6 @@ def show_event_post_admin(event_id=None, event_auth_token=None):
     daterange = enumerate(daterange)
 
     return render_template('event_manage.html', event=event, event_auth_token=event_auth_token, daterange=daterange, daterange24=daterange24)
+
+def get_event(id):
+    return models.Event.query.filter(models.Event.id == id).first()

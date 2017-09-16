a9f8b0220dcba64cc961e72525939f8ddc721651
Zane Cersovsky <zcersovsky@gmail.com>
2017-09-16 14:16:32 -0500

    Added tests for missing or empty event creation form fields. Refactored tests a bit.

--- tests/integration/event_tests.py
+++ tests/integration/event_tests.py
@@ -3,6 +3,10 @@ from urllib.parse import urlparse
 import event_planner
 from event_planner.models import *
 
+def make_slots():
+    """Helper func to make the timeslot part of the form data"""
+    return {"slot_%s" % i: "0" for i in range(48)}
+
 class IntegrationTestCase(unittest.TestCase):
     """Base class for all integration tests"""
     def setUp(self):
@@ -10,22 +14,25 @@ class IntegrationTestCase(unittest.TestCase):
         self.db = event_planner.db
         with event_planner.app.app_context():
             event_planner.db.create_all()
+        self.slots = make_slots()
+        self.text_members = ["eventname", "eventdescription", "adminname"]
+        self.form_members = self.text_members + ["date"]
     def tearDown(self):
         with event_planner.app.app_context():
             event_planner.db.drop_all()
-class WhenAnEventIsCreated(IntegrationTestCase):
+    def valid_data(self):
+        """Returns a valid dictionary of form data"""
+        data = {el: "testData" for el in self.text_members}
+        data.update(self.slots)
+        data["date"] = "12/31/2101"
+        return data
+
+class WhenAnEventIsCreatedSucessfully(IntegrationTestCase):
 
     def setUp(self):
         super().setUp()
-        self.slots = {"slot_%s" % i: "0" for i in range(48)}
         self.slots["slot_23"] = "1"
-        data=dict(
-            eventname="testevent",
-            eventdescription="testdescription",
-            adminname="testadmin",
-            date="11/21/201",
-            )
-        data.update(self.slots)
+        data=self.valid_data()
         self.res = self.app.post("/new", data=data)
     def test_status_code(self):
         """Should redirect (code 302)"""
@@ -41,9 +48,43 @@ class WhenAnEventIsCreated(IntegrationTestCase):
         """The new event should be accessible from /event/<id>"""
         res = self.app.get("/event/1")
         self.assertEqual(res.status_code, 200)
+
 class BeforeAnEventIsCreated(IntegrationTestCase):
-    
+
     def test_nonexist(self):
         """Should return a 404"""
         res = self.app.get("/event/3")
         self.assertEqual(res.status_code, 404)
+
+class CreatingAnEventWith(IntegrationTestCase):
+
+    def setUp(self):
+        super().setUp()
+    def test_missing_members(self):
+        """Should return 400"""
+        for element in self.form_members:
+            with self.subTest(i=element):
+                res = self.missing(element)
+                self.assertEqual(res.status_code, 400)
+    def test_missing_timeslots(self):
+        """Should return 400"""
+        data = {el: "testData" for el in self.form_members}
+        res = self.app.post("/new", data=data)
+        self.assertEqual(res.status_code, 400)
+    def test_empty_members(self):
+        """Should return 400"""
+        self.form_members.remove("eventdescription") #eventdescription is allowed to be empty
+        for element in self.form_members:
+            with self.subTest(i=element):
+                res = self.empty(element)
+                self.assertEqual(res.status_code, 400)
+    def missing(self, element):
+        """Executes a form submission, minus the given form member"""
+        data = self.valid_data()
+        del data[element]
+        return self.app.post("/new", data=data)
+    def empty(self, element):
+        """Executes a form submissing with the given field as empty string"""
+        data = self.valid_data()
+        data[element] = ""
+        return self.app.post("/new", data=data)

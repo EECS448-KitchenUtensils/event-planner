4cfba7386d75117817eb5b6acfbf96d80ea933b2
Zane Cersovsky <zcersovsky@gmail.com>
2017-09-16 16:15:00 -0500

    Reformatted the inline documentation in models

--- src/event_planner/models/Event.py
+++ src/event_planner/models/Event.py
@@ -5,26 +5,48 @@ from . Participant import Participant
 class Event(db.Model):
     """
     Database model for events
-    
-    Fields:
-        * id: INTEGER PRIMARY KEY
-        * title: TEXT
-        * description: TEXT
-        * date: DATE
-        * admin_link: TEXT
-    Relationships:
-        * participants: 1 -> * Participant
     """
     id = db.Column(db.Integer, primary_key=True)
+    """
+    The primary key for the database
+    
+    **Type:** INTEGER PRIMARY KEY
+    """
     title = db.Column(db.Text)
+    """
+    The title of this event
+
+    **Type:** TEXT
+    """
     description = db.Column(db.Text)
+    """
+    The description of this event
+
+    **Type:** TEXT
+    """
     date = db.Column(db.Date)
+    """
+    The date that this event takes place on
+
+    **Type:** DATE
+    """
     admin_link = db.Column(db.Text)
+    """
+    The *magic* code that authenicates the admin for this event
+
+    **Type:** TEXT
+    """
     participants = db.relationship("Participant")
-    def __init__(self, title, description, date, admin_name):
-        """Creates a new Event instance"""
+    """
+    Relationship to the participants of this event
+
+    **Related Model:** `event_planner.models.Participant`
+    """
+    def __init__(self, title, description, date):
+        """
+        Creates a new `Event` instance
+        """
         self.title = title
         self.description = description
         self.date = date
         self.admin_link = "".join([random.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(16)])
-        self.admin_name = admin_name
--- src/event_planner/models/Participant.py
+++ src/event_planner/models/Participant.py
@@ -2,23 +2,47 @@ from .. import db
 class Participant(db.Model):
     """
     Database model for participants
-
-    Fields:
-        * id: INTEGER PRIMARY KEY
-        * event_id: INTEGER FOREIGN KEY
-        * name: TEXT
-        * is_admin: BOOLEAN
-    Relationships:
-        * event: Parent Event for this Participant
-        * timeslots: 1 -> * Timeslots for this Participant
     """
     id = db.Column(db.Integer, primary_key=True)
+    """
+    The primary key for the database
+
+    **Type:** INTEGER PRIMARY KEY
+    """
     event_id = db.Column(db.Integer, db.ForeignKey("event.id"))
+    """
+    The key of the `Event` that this `Participant` belongs to
+
+    **Type:** INTEGER FOREIGN KEY
+    """
     event = db.relationship("Event")
+    """
+    The `Event` that this `Participant` belongs to
+
+    **Related Model:** `event_planner.models.Event`
+    """
     name = db.Column(db.Text)
+    """
+    The name of this `Participant`
+
+    **Type:** TEXT
+    """
     timeslots = db.relationship("Timeslot")
+    """
+    The `Timeslot`s that belong to this `Participant`
+
+    **Related Models:** `event_planner.models.Timeslot`
+    """
     is_admin = db.Column(db.Boolean)
+    """
+    Whether or not this `Participant` is an admin for its `Event`
+
+    **Type:** BOOLEAN
+    """
     def __init__(self, name, event, is_admin):
+        """
+        Creates a new `Participant` instance
+        """
         self.name = name
         self.event = event
         self.is_admin = is_admin

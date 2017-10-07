8094fe35eaf5dc52091c61e9506bf0e240899c0b
Andy <amonroe805@ku.edu>
2017-10-07 16:46:51 -0500

    Change participant foreign key to dateslot foreign key

--- src/event_planner/models/Timeslot.py
+++ src/event_planner/models/Timeslot.py
@@ -3,36 +3,36 @@ class Timeslot(db.Model):
     """
     Database model for a `Dateslot`'s `Timeslot`s
     """
-    
+
     id = db.Column(db.Integer, primary_key=True)
     """
     The primary key
     
     **Type:** INTEGER PRIMARY KEY
     """
-    
-    part_id = db.Column(db.Integer, db.ForeignKey("participant.id"))
+
+    date_id = db.Column(db.Integer, db.ForeignKey("dateslot.id"))
     """
     The id of the `Dateslot` that this `Timeslot` belongs to
     
     **Type:** INTEGER FOREIGN KEY
     """
-    
+
     time = db.Column(db.Time)
     """
     The `time` that this `Timeslot` started at
     
     **Type:** DATETIME
     """
-    
+
     dateslot = db.relationship("Dateslot")
     """
     Relationship to the `Dateslot` that this `Timeslot` belongs to
     
     **Related Model:** `event_planner.models.Dateslot`
     """
-    
-    def __init__(self, time, participant):
+
+    def __init__(self, time, dateslot):
         """Creates a new `Timeslot` instance"""
         self.time = time
-        self.participant = participant
+        self.dateslot = dateslot

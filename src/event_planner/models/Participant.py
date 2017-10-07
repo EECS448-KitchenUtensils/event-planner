26d18f0153a08a3e8611bcfadf77000323bce6a7
Andy <amonroe805@ku.edu>
2017-10-07 16:43:59 -0500

    Add vertical space (for my sanity)

--- src/event_planner/models/Participant.py
+++ src/event_planner/models/Participant.py
@@ -3,48 +3,56 @@ class Participant(db.Model):
     """
     Database model for participants
     """
+    
     id = db.Column(db.Integer, primary_key=True)
     """
     The primary key for the database
 
     **Type:** INTEGER PRIMARY KEY
     """
+
     event_id = db.Column(db.Integer, db.ForeignKey("event.id"))
     """
     The key of the `Event` that this `Participant` belongs to
 
     **Type:** INTEGER FOREIGN KEY
     """
+
     event = db.relationship("Event")
     """
     The `Event` that this `Participant` belongs to
 
     **Related Model:** `event_planner.models.Event`
     """
+
     name = db.Column(db.Text)
     """
     The name of this `Participant`
 
     **Type:** TEXT
     """
-    timeslots = db.relationship("Timeslot")
-    """
-    The `Timeslot`s that belong to this `Participant`
 
-    **Related Models:** `event_planner.models.Timeslot`
+    dateslots = db.relationship("Dateslot")
     """
+    The `Dateslot`s that belong to this `Participant`
+
+    **Related Models:** `event_planner.models.Dateslot`
+    """
+
     tasks = db.relationship("Task")
     """
     The `Task`s that belong to this `Participant`
 
     **Related Models:** `event_planner.models.Task`
     """
+
     is_admin = db.Column(db.Boolean)
     """
     Whether or not this `Participant` is an admin for its `Event`
 
     **Type:** BOOLEAN
     """
+
     def __init__(self, name, event, is_admin):
         """
         Creates a new `Participant` instance
--- src/event_planner/models/Task.py
+++ src/event_planner/models/Task.py
@@ -4,30 +4,35 @@ class Task(db.Model):
     """
     Database model for a `Participant`'s `Timeslot`s
     """
+    
     id = db.Column(db.Integer, primary_key=True)
     """
     The primary key
     
     **Type:** INTEGER PRIMARY KEY
     """
+    
     part_id = db.Column(db.Integer, db.ForeignKey("participant.id"))
     """
     The id of the `Participant` that this `Task` belongs to
     
     **Type:** INTEGER FOREIGN KEY
     """
+    
     task = db.Column(db.Text)
     """
     The `datetime` that this `Timeslot` started at
     
     **Type:** TEXT
     """
+    
     participant = db.relationship("Participant")
     """
     Relationship to the `Participant` that this `Task` belongs to
     
     **Related Model:** `event_planner.models.Participant`
     """
+    
     def __init__(self, task, participant):
         """Creates a new `Timeslot` instance"""
         self.task = task
